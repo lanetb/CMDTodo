07287f5ad9929236b1bb269a238d7515be30305c
lanetb <lanetb@dukes.jmu.edu>
2023-11-28 12:52:09 -0500

    Finished with basic cmd todo functionality

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,102 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"github.com/lanetb/CMDTodo"
+)
+
+const (
+	todoFile = ".todo.json"
+)
 
 func main(){
-	fmt.Println("Hello World")
+
+	add := flag.Bool("add", false, "Add a new task to the list")
+	complete := flag.Int("complete", 0, "Complete the task with the provided index")
+	del := flag.Int("del", 0, "Delete the task with the provided index")
+	list := flag.Bool("list", false, "List all tasks")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+	
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch{
+		case *add:
+			task, err := getInput(os.Stdin, flag.Args()...)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+
+			todos.Add(task)
+
+			err = todos.Store(todoFile)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+		case *complete > 0:
+			err := todos.Complete(*complete)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+
+			err = todos.Store(todoFile)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+		case *del > 0:
+			err := todos.Delete(*del)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+
+			err = todos.Store(todoFile)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+		case *list:
+			todos.Print()
+
+		default:
+			fmt.Fprintln(os.Stdout, "Invalid command")
+			os.Exit(1)
+	}
+}
+
+
+
+func getInput(r io.Reader, args ...string) (string, error){
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(scanner.Text()) == 0{
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return text, nil
 }
